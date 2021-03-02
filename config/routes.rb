24a3166f4193047aa0b9039448dd2ccaa2f38372
Daikichi1223 <abcdaiki1223@icloud.com>
2021-03-02 23:56:25 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :shops
   devise_for :users
   root to: "meals#index"
-  resources :meals, only: [:index]
+  resources :meals, only: [:index, :new]
 end
