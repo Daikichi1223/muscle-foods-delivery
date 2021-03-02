aa9d755806c25a14c5a366730d4aa312d10138ad
Daikichi1223 <abcdaiki1223@icloud.com>
2021-03-03 00:13:20 +0900

    ActiveStorageの導入

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,4 +6,18 @@ class MealsController < ApplicationController
   def new
     @meal = Meal.new
   end
+
+  def create
+    @meal = Meal.create(meal_params)
+    if @meal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def meal_params
+    params.require(:meal).permit(:name, :explanation, :genre_id, :protein, :fat, :carbo, :price, :image).merge(shop_id: current_shop.id)
+  end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,4 @@
 class Meal < ApplicationRecord
+  belongs_to :shop
+  has_one_attached :image
 end
