c530794e8cf73f5c5b6284ffe8982687f168b31a
Daikichi1223 <abcdaiki1223@icloud.com>
2021-03-08 11:20:18 +0900

    購入画面のビューの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,13 @@
 class OrdersController < ApplicationController
+before_action :authenticate_user!
+before_action :set_item, only: [:index, :create]
+
   def index
     
+  end
+
+  private
+  def set_item
+    @meal = Meal.find(params[:meal_id])
   end
 end
